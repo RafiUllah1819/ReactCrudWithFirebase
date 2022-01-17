e3c13f0b029e839e8668a583636897f63f30ec4a
rafidurrani <rafiullah372@gmail.com>
2022-01-18 02:56:23 +0500

    second commit

---
+++ src/Context/ContextState.js
@@ -0,0 +1,35 @@
+import React ,{useState , useEffect} from 'react';
+import { ContextNode } from './Context'; 
+import { db } from '../Config/FirebaseConfig';
+import { collection, onSnapshot } from "firebase/firestore";
+
+export const ContextState = () => {
+    const [persons, setPersons] = useState([]);
+
+    useEffect(() => {
+      const collectionRef = collection(db, "users");
+      const unsubscribe = onSnapshot(
+        collectionRef,
+        (snapshot) => {
+          if (!snapshot.empty) {
+            const updatedData = snapshot.docs.map((doc) => {
+              return { ...doc.data(), id: doc.id };
+            });
+            setPersons(updatedData);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      return () => unsubscribe();
+    }, []);
+    console.log("persons", persons);
+    return (
+        <div>
+            <ContextNode.Provider={}>
+
+            </ContextNode.Provider>
+        </div>
+    )
+}
